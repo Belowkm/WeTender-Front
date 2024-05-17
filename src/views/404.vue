b191f7c7b8f53061461be3929768c39d3547aafc
Belowkm <93333275+Belowkm@users.noreply.github.com>
2024-05-18 01:45:05 +0800

    Add files via upload

---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="404">
+        <div class="error-page">
+            <div class="error-desc">{{ msg }}</div>
+            <div class="error-handle">
+                <i class="el-icon-menu"></i>
+                <ul><router-link to="/home">返回首页</router-link></ul>        
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: '404',
+    data() {
+        return {
+            msg: '404'
+        }
+    }
+}
+</script>
