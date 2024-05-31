e17c37da52286a36761e253c56edf3112bde6482
Belowkm <belowkm@163.com>
2024-05-31 15:52:29 +0800

    test

---
+++ src/components/Aside.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="Aside">
+        <el-menu :default-active="activeIndex" class="el-menu-vertical-demo" @select="handleSelect">
+            <el-menu-item index="1">
+                <template #title>
+                    <i class="el-icon-menu"></i>
+                    <span>数据概览</span>
+                </template>
+            </el-menu-item>
+            <el-menu-item index="2">
+                <template #title>
+                    <i class="el-icon-document"></i>
+                    <span>项目管理</span>
+                </template>
+            </el-menu-item>
+            <el-menu-item index="3">
+                <template #title>
+                    <i class="el-icon-setting"></i>
+                    <span>预算管理</span>
+                </template>
+            </el-menu-item>
+        </el-menu>
+    </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+const activeIndex = ref('1')
+const handleSelect = (key, keyPath) => {
+    if (key === '1') {
+        useRouter().push('/dashboard')
+    }
+    console.log(key, keyPath)
+}
+</script>
+<style></style>
--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,89 +1,7 @@
 <template>
     <el-container class="dashboard">
         <el-aside width="200px">
-            <el-menu>
-                <el-sub-menu>
-                    <template #title>
-                        <i class="el-icon-menu"></i>
-                        乡级政府界面
-                    </template>
-                    <el-menu-item >
-                        <i class="el-icon-menu"></i>
-                        数据概览
-                    </el-menu-item>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        项目列表
-                    </el-menu-item>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        项目管理
-                    </el-menu-item>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        项目审核
-                    </el-menu-item>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        数据导出
-                    </el-menu-item>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        预算管理
-                    </el-menu-item>
-                </el-sub-menu>
-            </el-menu>
-
-            <el-menu>
-                <el-sub-menu>
-                    <template #title>
-                        <i class="el-icon-menu"></i>
-                        县级政府界面
-                    </template>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        数据概览
-                    </el-menu-item>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        项目列表
-                    </el-menu-item>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        项目管理
-                    </el-menu-item>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        项目审核
-                    </el-menu-item>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        数据导出
-                    </el-menu-item>
-                    <el-menu-item>
-                        <i class="el-icon-menu"></i>
-                        预算管理
-                    </el-menu-item>
-                </el-sub-menu>
-            </el-menu>
-            
-            <el-menu>
-                <el-sub-menu>
-                    <template #title>
-                        <i class="el-icon-menu"></i>
-                        企业界面
-                    </template>
-                </el-sub-menu>
-            </el-menu>
-            
-            <el-menu>
-                <el-sub-menu disabled>
-                    <template #title>
-                        <i class="el-icon-menu"></i>
-                        银行界面
-                    </template>
-                </el-sub-menu>
-            </el-menu>
+            <Aside />
         </el-aside>
         <el-main>
 
@@ -91,16 +9,8 @@
     </el-container>
 </template>
 <script lang="ts" setup>
+import Aside from '../components/Aside.vue'
 </script>
 <style>
-.dashboard {
-    position: fixed;
-    width: 100%;
-    z-index: 3000;
-    top: 60px;
-    left: 0;
-    right: 0;
-    margin: 0 auto;
-    overflow: hidden;
-}
+
 </style>
