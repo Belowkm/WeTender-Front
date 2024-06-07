0ed8ebab1c1fb5881eded30a3bdd336aa9a76415
Belowkm <belowkm@163.com>
2024-06-08 03:31:12 +0800

    诶嘿~

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,42 +1,21 @@
 <template>
-    <div class="test">
-    <span>这是一个测试界面</span>
-    <br>
-        <el-button @click="show = !show">点击按钮</el-button>
-
-        <div style="display: flex; margin-top: 20px; height: 100px">
-            <transition name="el-fade-in-linear">
-                <div v-show="show" class="transition-box">WeTender</div>
-            </transition>
-            <transition name="el-fade-in">
-                <div v-show="show" class="transition-box">学生团队</div>
-            </transition>
-        </div>
+    <div class="admin-backend">
+        <el-button type="danger" @click="goToBackend">
+            前往Super管理员界面
+        </el-button>
     </div>
 </template>
-
 <script lang="ts" setup>
-import { ref } from 'vue'
-const show = ref(true)
+import { ElMessage, ElMessageBox } from 'element-plus'
+const goToBackend = () => {
+    ElMessageBox.confirm('前往Super管理员界面前，请确认拥有管理员账号和密码。', '提示', {
+        confirmButtonText: '确认',
+        cancelButtonText: '取消',
+        type: 'warning',
+    })
+        .then(() => {
+            window.open('http://47.106.226.160:5000/')
+        })
+}
 </script>
-
-<style>
-.test{
-    position: fixed;
-    top: 40%;
-    left: 40%;
-}
-.transition-box {
-    margin-bottom: 10px;
-    width: 200px;
-    height: 100px;
-    border-radius: 4px;
-    background-color: #409eff;
-    text-align: center;
-    color: #fff;
-    padding: 40px 20px;
-    box-sizing: border-box;
-    margin-right: 20px;
-}
-
-</style>
+<style></style>
